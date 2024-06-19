169fa04672b627bb712f156bd08da62091088409
guillaume <guillaume.goubel.pro@gmail.com>
2024-06-19 20:51:20 +0200

    exo

--- 01_concevoir_scripts_python/demo/workshop.py
+++ 01_concevoir_scripts_python/demo/workshop.py
@@ -34,41 +34,57 @@
 # print(f"le serveur est en maintenance : << { bool(isOnMaintenance) } >> ")
 
 #! Bande passante
-needs = []
-bandwidth = input("quelle est la bande passante totale disponible dans le data center (en Gbps) ? >> ")
+needs_list = []
+needs_to_array_sorted = []
+services_ok = []
+services_nok = []
+bandwidth = int(input("quelle est la bande passante totale disponible dans le data center (en Gbps) ? >> "))
 
 while True:
-    bandwidth_by_service = input("1.service : 2.priorité du service : 3.besoin bande passante ? >> taper 'fin' pour terminer la liste >> ")
+    bandwidth_by_service = input("1.service : 2.priorité : 3.besoin Gbps ? >> (or 'fin') >> ")
     if bandwidth_by_service == 'fin':
 
-        for need in needs:
-            print("######## Vos services déclarés ########")
-            print(need)
+        # Résumé des services déclarés :
+        print("######## Vos services déclarés ########")
+        bandwidth_total = 0
+        for need in needs_to_array_sorted:
+            print(f">> {need[0]} : {need[2]}")
+            bandwidth_total += int(need[2])
+        print("#######################################")
+            
+        # Calcul de la bande passante :
+        if int(bandwidth_total) > bandwidth:
+            print(">> WAIT -- Il n'y a pas assez de bande passante dispo, nous devons prioriser ...")
+            for need in needs_to_array_sorted:
+                
+                if bandwidth > 0:
 
-        break
+                    # calcul du restant de la bande passante
+                    bandwidth -= int(need[2])
+
+                    # dispatch des services OK / NOK
+                    if bandwidth > 0:
+                        services_ok.append(need[0])
+                    else:
+                        services_nok.append(need[0])                        
+                else:
+                    services_nok.append(need[0])
+            
+            # Affichage final
+            print("#######################################")
+            print(">> SERVICES OK :")
+            for service in services_ok:
+                print(f"> {service}")
+            print(">> SERVICES NOK :")
+            for service in services_nok:
+                print(f"> {service}")
+            break
+        else:
+            print(">> SUCCESS -- La bande passante est suffisante pour tous les services déclarés")
+            break
+
     else:
- 
-        foo = bandwidth_by_service.split(" : ")
-        needs.append(tuple(foo))
-        print(needs)
-
-        toto = sorted(needs, key=lambda priority: priority[1])
-        print(toto)
-
-# Pour  chaque  service  (par  exemple,  Web,  Email,  Base  de  données),  demandez  à
-# l'utilisateur d'entrer la priorité du service (sur une échelle de 1 à 3, 1 étant le plus haut).
-# Demandez ensuite la demande de bande passante pour chaque service (en Gbps).
-# En  fonction  de  la  priorité,  allouez  la  bande  passante  disponible  aux  services,  en
-# commençant par le service de priorité 1. Si la bande passante totale est insuffisante,
-# répartissez-la proportionnellement en fonction de la priorité.
-# Affichez la bande passante allouée à chaque service.
-
-
-# student_tuples = [
-#     ('john', 'A', 15),
-#     ('jane', 'B', 12),
-#     ('dave', 'B', 10),
-# ]
-# sorted(student_tuples, key=lambda student: student[2])
-
-
+        needs_to_array = bandwidth_by_service.split(" : ")
+        needs_to_array_convert = [needs_to_array[0], int(needs_to_array[1]), int(needs_to_array[2])]
+        needs_list.append(tuple(needs_to_array_convert))
+        needs_to_array_sorted = sorted(needs_list, key=lambda x: (x[1], x[2]))
